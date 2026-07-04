6bc52b723f991fccedce7df6470a76f28cb4059c
luis felipe amador bravo <luigifelipe333@gmail.com>
2026-07-03 22:27:32 -0600

    Se agrega la logica y consumo de la PokeApi al agregar un pokemon al Ranking

--- Services/PokemonService.cs
+++ Services/PokemonService.cs
@@ -24,11 +24,62 @@ namespace MyPokemonRankingApi.Services
 
         public async Task<Pokemon> AddToRankingAsync(CreatePokemonDto createDto)
         {
-            // 1. TODO: Consumir la PokéAPI usando el createDto.PokemonApiId
+            // Consumir la PokéAPI usando el createDto.PokemonApiId
+            var url = $"https://pokeapi.co/api/v2/pokemon/{createDto.PokemonApiId}";
+
+            var pokeData = await _httpClient.GetFromJsonAsync<PokeApiPokemonResponse>(url);
+
+            if (pokeData == null)
+            {
+                throw new Exception("No se encontró el Pokémon en la PokéAPI externa.");
+            }
+
+            var tipos = pokeData.types
+                .OrderBy(t => t.slot)
+                .Select(t => t.type.name)
+                .ToList();
+
+            string primaryType = tipos.FirstOrDefault() ?? "Unknown";
+            string? secondaryType = tipos.Count > 1 ? tipos[1] : null;
+
             // 2. TODO: Aplicar la regla de negocio para recorrer las posiciones
+            
+            var pokemonAfectados = await _repository.GetAllAsync(); //Traer todos los pokemones para poder recorrerlos
+
+            var paraRecorrer = pokemonAfectados
+                .Where(p => p.Position >= createDto.Position)
+                .OrderByDescending(p => p.Position); //Ordenamos de mayor a menor para poder recorrerlos y sumarle 1 a su posición
+
+            foreach (var poke in paraRecorrer) //Recorremos los pokemones que tienen una posición mayor o igual a la que queremos insertar
+            {
+                poke.Position += 1;
+                _repository.Update(poke);
+            }
+
             // 3. TODO: Guardar en la base de datos a través del repositorio
-            throw new NotImplementedException();
+
+            var nuevoPokemon = new Pokemon
+            {
+                PokemonApiId = createDto.PokemonApiId,
+                Position = createDto.Position,
+                Name = char.ToUpper(pokeData.name[0]) + pokeData.name.Substring(1), // Capitalizamos la primera letra del nombre
+                PrimaryType = primaryType,
+                SecondaryType = secondaryType
+            };
+
+            // 4. Guardamos el nuevo Pokémon y confirmamos todos los cambios en la BD
+            await _repository.AddAsync(nuevoPokemon);
+            await _repository.SaveChangesAsync();
+
+            return nuevoPokemon;
         }
 
     }
+
+    // Clases para mapear la respuesta de la PokéAPI
+    public record PokeApiPokemonResponse(string name, List<PokeApiTypeSlot> types);
+    public record PokeApiTypeSlot(int slot, PokeApiType type);
+    public record PokeApiType(string name);
+
+
 }
